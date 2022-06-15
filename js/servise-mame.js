bbf4aeb71f5506a5f5cf517b27e1bd7f8bba29a8
Oz Ansbacher <ozansbacher@gmail.com>
2022-06-15 12:16:06 +0300

    upload 02

--- js/servise-mame.js
+++ js/servise-mame.js
@@ -1,1 +1,25 @@
 'use strict'
+
+var gNumImgs = 24
+var gImgs = []
+const gKeywords = ['funny', 'bad', 'nice', 'good']
+
+
+function creatImgs() {
+    for (let i = 1; i <= gNumImgs; i++) {
+        const keywords = gKeywords[getRandomInt(0, gKeywords.length)]
+        gImgs.push(creatImg(i, keywords))
+    }
+}
+
+function creatImg(id, argument) {
+    return {
+        id,
+        url: `img/img${id}.jpg`,
+        keywords: argument
+    }
+}
+
+function getMeme(idx = 0) {
+    return gImgs[idx]
+}
